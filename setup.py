ed5e06aa38bb11447be6c28e9497bd09aa2d78af
Vilgiforts <vilgiforts@gmail.com>
2025-10-19 23:21:43 +0300

    gitignore

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="oscillogram",  # Укажите имя вашей библиотеки
-    version="0.1.0",
-    packages=find_packages(),  # Автоматически находит все пакеты
-)
