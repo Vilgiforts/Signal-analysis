32ea4793bdebd53d210d8407c8c648db2a8ff763
root <root@12386.koara.live>
2025-12-08 09:02:12 +0100

    Исправление ошибки в вычислениии погрешностей

--- oscillogram/eror_funks.py
+++ oscillogram/eror_funks.py
@@ -4,22 +4,25 @@ import scipy.stats as sps
 def confidence_interval(
     array: list[float | int] | tuple[float | int],
     confidence_probability: float = 0.95,
-    absolute_error: int | float = None,
-    relative_error: int | float = None,
+    absolute_error: int | float = 0,
+    relative_error: int | float = 0,
 ):
-    t = sps.t.ppf(1 / 2 + confidence_probability / 2, df=len(array) - 1)
-    t_inf = sps.t.ppf(1 / 2 + confidence_probability / 2, df=10**6)
-    mean_valeu = sum((abs(value) for value in array)) / len(array)
-    S_0 = (
-        sum(((abs(valeu) - mean_valeu) ** 2 for valeu in array))
-        / (len(array) * (len(array) - 1))
-    ) ** 0.5
-    rez = (
-        (mean_valeu, ((t * S_0) ** 2 + (t_inf * absolute_error / 3) ** 2) ** 0.5)
-        if absolute_error
-        else (
+    if len(array) > 1:
+        t = sps.t.ppf(1 / 2 + confidence_probability / 2, df=len(array) - 1)
+        t_inf = sps.t.ppf(1 / 2 + confidence_probability / 2, df=10**6)
+        mean_valeu = sum(array) / len(array)
+        S_0 = (
+            sum(((valeu - mean_valeu) ** 2 for valeu in array))
+            / (len(array) * (len(array) - 1))
+        ) ** 0.5
+        rez = (
             mean_valeu,
-            ((t * S_0) ** 2 + (t_inf * relative_error * mean_valeu / 3) ** 2) ** 0.5,
+            (
+                (t * S_0) ** 2
+                + (t_inf * (absolute_error + relative_error * mean_valeu) / 3) ** 2
+            )
+            ** 0.5,
         )
-    )
-    return rez
+        return rez
+    else:
+        return (array[0], absolute_error + relative_error * array[0])
